095a6179888f957f2d2481ccff8889d30fea13a2
湫曗 <18817832003@qq.com>
2024-03-26 19:21:53 +0800

    feat: 85%

--- MLP_TEST.py
+++ MLP_TEST.py
@@ -4,19 +4,19 @@ class AlexModel(nn.Module):#定义AlexNet的模型
     def __init__(self):#width_mult为输入图像的尺寸缩放比默认为1。
         super(AlexModel,self).__init__()#继承父类的初始化方法
         self.layer0=nn.Sequential(
-            nn.Linear(50176,512),
+            nn.Linear(50176,10),
             nn.ReLU(inplace=True),
             nn.Dropout(0.5),
         )
         self.layer1=nn.Sequential(
-            nn.Linear(512,512),
+            nn.Linear(10,10),
             nn.ReLU(inplace=True),
             nn.Dropout(0.5),
         )
        
         #定义全连接层,用于1-10分类得分
         self.fc=nn.Sequential(#nn.Dropout(0.5),
-                              nn.Linear(512,10),
+                              nn.Linear(10,10),
         )
 
     def forward(self, x):#前向传播
