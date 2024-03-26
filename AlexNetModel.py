4cc667332247b42772e567bbd36b5f3955a306bd
湫曗 <18817832003@qq.com>
2024-03-26 21:36:28 +0800

    feat: 90%

--- AlexNetModel.py
+++ AlexNetModel.py
@@ -8,12 +8,12 @@ class AlexModel(nn.Module):#定义AlexNet的模型
     def __init__(self):#width_mult为输入图像的尺寸缩放比默认为1。
         super(AlexModel,self).__init__()#继承父类的初始化方法
         self.layer1=nn.Sequential(
-            nn.Conv2d(in_channels=1,out_channels=2,kernel_size=3,stride=1),#(224 - 3 + 2*2) / 3 + 1 = 55包含96个大小为11×11的滤波器（其实是11×11×3），卷积步长为4，因此第一层输出大小为55×55×96，padding=2；
+            nn.Conv2d(in_channels=1,out_channels=1,kernel_size=3,stride=1),#(224 - 3 + 2*2) / 3 + 1 = 55包含96个大小为11×11的滤波器（其实是11×11×3），卷积步长为4，因此第一层输出大小为55×55×96，padding=2；
         )
 
         #定义全连接层,用于1-10分类得分
         self.fc=nn.Sequential(#nn.Dropout(0.5),
-                              nn.Linear(1352, 10))
+                              nn.Linear(676, 10))
 
     def forward(self, x):#前向传播
 
